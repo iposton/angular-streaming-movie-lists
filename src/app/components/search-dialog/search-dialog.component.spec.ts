b7283b6644991cf940c32457e4d103ba6eb3d2af
Ian Poston <ianposton@Ians-MacBook-Air.local>
2021-08-27 16:08:52 -0700

    udate 2

--- src/app/components/search-dialog/search-dialog.component.spec.ts
+++ src/app/components/search-dialog/search-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SearchDialogComponent } from './search-dialog.component';
 
@@ -6,7 +6,7 @@ describe('SearchDialogComponent', () => {
   let component: SearchDialogComponent;
   let fixture: ComponentFixture<SearchDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchDialogComponent ]
     })
