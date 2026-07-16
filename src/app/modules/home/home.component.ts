ce0ce674186ac58ad0e2a2d9d00042d517059534
iposton <ianposton@sbcglobal.net>
2026-07-15 22:11:38 -0700

    fix load more

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -42,19 +42,19 @@ export class HomeComponent implements OnInit {
     { value: '10768', label: 'War & Politics' },
     { value: '37', label: 'Western' }
   ]
-  public netFlix: Array<any>
-  public amazon: Array<any>
-  public disney: Array<any>
-  public hbo: Array<any>
-  public hulu: Array<any>
-  public apple: Array<any>
-  public hboTv: Array<any>
-  public huluTv: Array<any>
-  public appleTv: Array<any>
+  public netflixMovies: Array<any> = []
+  public primeMovies: Array<any> = []
+  public disneyMovies: Array<any> = []
+  public maxMovies: Array<any> = []
+  public huluMovies: Array<any> = []
+  public appleMovies: Array<any> = []
+  public maxTvShows: Array<any> = []
+  public huluTvShows: Array<any> = []
+  public appleTvShows: Array<any> = []
  
-  public netFlixTv: Array<any>;
-  public amazonTv: Array<any>;
-  public disneyTv: Array<any>;
+  public netflixTvShows: Array<any> = [];
+  public primeTvShows: Array<any> = [];
+  public disneyTvShows: Array<any> = [];
   public error: any;
   public info: boolean = false;
 
@@ -103,7 +103,7 @@ export class HomeComponent implements OnInit {
   public selectedMovie: any;
   public selected: any;
   public year: string = '26';
-  public provider: string = 'npy';
+  public providerGroup: string = 'npy';
   public genre: string = '';
   public related: Array<any>;
   public relatedDetails: Array<any>;
@@ -146,10 +146,10 @@ export class HomeComponent implements OnInit {
     this.dataService.type = cat
     this.type = cat
     this.closeGenreMenu()
-    if (this.netFlix.length === 0 || this.netFlixTv.length === 0 || this.year != '22' || this.genre != '') {
+    if (this.netflixMovies.length === 0 || this.netflixTvShows.length === 0 || this.year != '22' || this.genre != '') {
       this.genre = ''
       this.defaultYear = '26'
-      this.provider = 'npy'
+      this.providerGroup = 'npy'
       this.year = '26'
       this.loadItems()
     }
@@ -158,25 +158,23 @@ export class HomeComponent implements OnInit {
   public onYearChange(year: string) {
     if (this.year != year) {
       this.year = year
-      this.provider = 'npy'
+      this.providerGroup = 'npy'
       this.closeGenreMenu()
       this.loadItems()
     }
     
   }
 
-  public onProviderChange(provider: string) {
-    if (this.provider != provider) {
+  public onProviderChange(providerGroup: string) {
+    if (this.providerGroup != providerGroup) {
       this.util.loadingMore = true
-      this.netFlixTv = null
-      //this.genre = '';
-      this.provider = provider
+      this.providerGroup = providerGroup
       this.loadItems()
     } 
    }
 
   public setGenre(genre: string) {
-    this.provider = 'npy'
+    this.providerGroup = 'npy'
     if (this.genre != genre) {
       this.genre = genre
       this.loadItems()
@@ -205,7 +203,7 @@ export class HomeComponent implements OnInit {
     }
 
     this.genre = ''
-    this.provider = 'npy'
+    this.providerGroup = 'npy'
     this.loadItems()
     this.closeGenreMenu()
   }
@@ -229,61 +227,66 @@ export class HomeComponent implements OnInit {
 
   public loadItems() {
     this.submitting = this.util.loadingMore === true ? false : true
-    //this.type = 'movies';
-    this.dataService.getData(this.year, this.genre, this.provider).subscribe(res => {
+    const requestedProviderGroup = this.providerGroup
+    this.dataService.getData(this.year, this.genre, this.providerGroup).subscribe(res => {
       console.log(res, 'movies from server');
-      this.netFlix = res[0].nfMovies;
-      this.amazon = res[0].amzMovies;
-      this.disney = res[0].disneyMovies;
-      this.netFlixCredits = res[0].nfCredits;
-      this.netFlixDetails = res[0].nfDetails;
-      this.amazonCredits = res[0].amzCredits;
-      this.amazonDetails = res[0].amzDetails;
-      this.disneyCredits = res[0].disneyCredits;
-      this.disneyDetails = res[0].disneyDetails;
+      if (requestedProviderGroup === 'npy' || requestedProviderGroup === 'nkpkd') {
+        this.netflixMovies = res[0].nfMovies;
+        this.primeMovies = res[0].amzMovies;
+        this.disneyMovies = res[0].disneyMovies;
+        this.netFlixCredits = res[0].nfCredits;
+        this.netFlixDetails = res[0].nfDetails;
+        this.amazonCredits = res[0].amzCredits;
+        this.amazonDetails = res[0].amzDetails;
+        this.disneyCredits = res[0].disneyCredits;
+        this.disneyDetails = res[0].disneyDetails;
 
-      this.hbo = res[0].hboMovies;
-      this.hulu = res[0].huluMovies;
-      this.apple = res[0].appleMovies;
-      this.hboCredits = res[0].hboCredits;
-      this.hboDetails = res[0].hboDetails;
-      this.huluCredits = res[0].huluCredits;
-      this.huluDetails = res[0].huluDetails;
-      this.appleCredits = res[0].appleCredits;
-      this.appleDetails = res[0].appleDetails;
+        this.netflixTvShows = res[0].nfTv;
+        this.primeTvShows = res[0].amzTv;
+        this.disneyTvShows = res[0].disneyTv;
+        this.netFlixTvCredits = res[0].nfTvCredits;
+        this.netFlixTvDetails = res[0].nfTvDetails;
+        this.amazonTvCredits = res[0].amzTvCredits;
+        this.amazonTvDetails = res[0].amzTvDetails;
+        this.disneyTvCredits = res[0].disneyTvCredits;
+        this.disneyTvDetails = res[0].disneyTvDetails;
+      }
 
-      this.rank(this.netFlix)
-      this.rank(this.amazon)
-      this.rank(this.disney)
-      this.rank(this.hbo)
-      this.rank(this.hulu)
-      this.rank(this.apple)
+      if (requestedProviderGroup === 'hha') {
+        this.maxMovies = res[0].hboMovies;
+        this.huluMovies = res[0].huluMovies;
+        this.appleMovies = res[0].appleMovies;
+        this.hboCredits = res[0].hboCredits;
+        this.hboDetails = res[0].hboDetails;
+        this.huluCredits = res[0].huluCredits;
+        this.huluDetails = res[0].huluDetails;
+        this.appleCredits = res[0].appleCredits;
+        this.appleDetails = res[0].appleDetails;
 
-      this.netFlixTv = res[0].nfTv;
-      this.amazonTv = res[0].amzTv;
-      this.disneyTv = res[0].disneyTv;
-      this.netFlixTvCredits = res[0].nfTvCredits;
-      this.netFlixTvDetails = res[0].nfTvDetails;
-      this.amazonTvCredits = res[0].amzTvCredits;
-      this.amazonTvDetails = res[0].amzTvDetails;
-      this.disneyTvCredits = res[0].disneyTvCredits;
-      this.disneyTvDetails = res[0].disneyTvDetails;
+        this.maxTvShows = res[0].hboTv;
+        this.huluTvShows = res[0].huluTv;
+        this.appleTvShows = res[0].appleTv;
+        this.hboTvCredits = res[0].hboTvCredits;
+        this.hboTvDetails = res[0].hboTvDetails;
+        this.huluTvCredits = res[0].huluTvCredits;
+        this.huluTvDetails = res[0].huluTvDetails;
+        this.appleTvCredits = res[0].appleTvCredits;
+        this.appleTvDetails = res[0].appleTvDetails;
+      }
 
-      this.hboTv = res[0].hboTv;
-      this.huluTv = res[0].huluTv;
-      this.appleTv = res[0].appleTv;
-      this.hboTvCredits = res[0].hboTvCredits;
-      this.hboTvDetails = res[0].hboTvDetails;
-      this.huluTvCredits = res[0].huluTvCredits;
-      this.huluTvDetails = res[0].huluTvDetails;
-      this.appleTvCredits = res[0].appleTvCredits;
-      this.appleTvDetails = res[0].appleTvDetails;
-      this.rank(this.netFlixTv);
-      this.rank(this.amazonTv);
-      this.rank(this.disneyTv);
-      this.rank(this.hboTv);
-      this.rank(this.huluTv);
-      this.rank(this.appleTv);
+      this.rank(this.netflixMovies)
+      this.rank(this.primeMovies)
+      this.rank(this.disneyMovies)
+      this.rank(this.maxMovies)
+      this.rank(this.huluMovies)
+      this.rank(this.appleMovies)
+
+      this.rank(this.netflixTvShows);
+      this.rank(this.primeTvShows);
+      this.rank(this.disneyTvShows);
+      this.rank(this.maxTvShows);
+      this.rank(this.huluTvShows);
+      this.rank(this.appleTvShows);
 
       this.sortData()
     })
@@ -297,33 +300,33 @@ export class HomeComponent implements OnInit {
 
   public sortData() {
     try {
-      this.util.relatedInfo(this.netFlix, this.netFlixDetails, 'details', 'movies', this.provider, 1)
-      this.util.relatedInfo(this.netFlix, this.netFlixCredits, 'credits', 'movies', this.provider, 1)
-      this.util.relatedInfo(this.amazon, this.amazonDetails, 'details', 'movies', this.provider, 2)
-      this.util.relatedInfo(this.amazon, this.amazonCredits, 'credits', 'movies', this.provider, 2)
-      this.util.relatedInfo(this.disney, this.disneyDetails, 'details', 'movies', this.provider, 3)
-      this.util.relatedInfo(this.disney, this.disneyCredits, 'credits', 'movies', this.provider, 3)
+      this.util.relatedInfo(this.netflixMovies, this.netFlixDetails, 'details', 'movies', this.providerGroup, 1)
+      this.util.relatedInfo(this.netflixMovies, this.netFlixCredits, 'credits', 'movies', this.providerGroup, 1)
+      this.util.relatedInfo(this.primeMovies, this.amazonDetails, 'details', 'movies', this.providerGroup, 2)
+      this.util.relatedInfo(this.primeMovies, this.amazonCredits, 'credits', 'movies', this.providerGroup, 2)
+      this.util.relatedInfo(this.disneyMovies, this.disneyDetails, 'details', 'movies', this.providerGroup, 3)
+      this.util.relatedInfo(this.disneyMovies, this.disneyCredits, 'credits', 'movies', this.providerGroup, 3)
 
-      this.util.relatedInfo(this.hbo, this.hboDetails, 'details', 'movies', this.provider, 1)
-      this.util.relatedInfo(this.hbo, this.hboCredits, 'credits', 'movies', this.provider, 1)
-      this.util.relatedInfo(this.hulu, this.huluDetails, 'details', 'movies', this.provider, 2)
-      this.util.relatedInfo(this.hulu, this.huluCredits, 'credits', 'movies', this.provider, 2)
-      this.util.relatedInfo(this.apple, this.appleDetails, 'details', 'movies', this.provider, 3)
-      this.util.relatedInfo(this.apple, this.appleCredits, 'credits', 'movies', this.provider, 3)
+      this.util.relatedInfo(this.maxMovies, this.hboDetails, 'details', 'movies', this.providerGroup, 1)
+      this.util.relatedInfo(this.maxMovies, this.hboCredits, 'credits', 'movies', this.providerGroup, 1)
+      this.util.relatedInfo(this.huluMovies, this.huluDetails, 'details', 'movies', this.providerGroup, 2)
+      this.util.relatedInfo(this.huluMovies, this.huluCredits, 'credits', 'movies', this.providerGroup, 2)
+      this.util.relatedInfo(this.appleMovies, this.appleDetails, 'details', 'movies', this.providerGroup, 3)
+      this.util.relatedInfo(this.appleMovies, this.appleCredits, 'credits', 'movies', this.providerGroup, 3)
 
-      this.util.relatedInfo(this.netFlixTv, this.netFlixTvDetails, 'details', 'tv', this.provider, 1)
-      this.util.relatedInfo(this.netFlixTv, this.netFlixTvCredits, 'credits', 'tv', this.provider, 1)
-      this.util.relatedInfo(this.amazonTv, this.amazonTvDetails, 'details', 'tv', this.provider, 2)
-      this.util.relatedInfo(this.amazonTv, this.amazonTvCredits, 'credits', 'tv', this.provider, 2)
-      this.util.relatedInfo(this.disneyTv, this.disneyTvDetails, 'details', 'tv', this.provider, 3)
-      this.util.relatedInfo(this.disneyTv, this.disneyTvCredits, 'credits', 'tv', this.provider, 3)
+      this.util.relatedInfo(this.netflixTvShows, this.netFlixTvDetails, 'details', 'tv', this.providerGroup, 1)
+      this.util.relatedInfo(this.netflixTvShows, this.netFlixTvCredits, 'credits', 'tv', this.providerGroup, 1)
+      this.util.relatedInfo(this.primeTvShows, this.amazonTvDetails, 'details', 'tv', this.providerGroup, 2)
+      this.util.relatedInfo(this.primeTvShows, this.amazonTvCredits, 'credits', 'tv', this.providerGroup, 2)
+      this.util.relatedInfo(this.disneyTvShows, this.disneyTvDetails, 'details', 'tv', this.providerGroup, 3)
+      this.util.relatedInfo(this.disneyTvShows, this.disneyTvCredits, 'credits', 'tv', this.providerGroup, 3)
 
-      this.util.relatedInfo(this.hboTv, this.hboTvDetails, 'details', 'tv', this.provider, 1)
-      this.util.relatedInfo(this.hboTv, this.hboTvCredits, 'credits', 'tv', this.provider, 1)
-      this.util.relatedInfo(this.huluTv, this.huluTvDetails, 'details', 'tv', this.provider, 2)
-      this.util.relatedInfo(this.huluTv, this.huluTvCredits, 'credits', 'tv', this.provider, 2)
-      this.util.relatedInfo(this.appleTv, this.appleTvDetails, 'details', 'tv', this.provider, 3)
-      this.util.relatedInfo(this.appleTv, this.appleTvCredits, 'credits', 'tv', this.provider, 3)
+      this.util.relatedInfo(this.maxTvShows, this.hboTvDetails, 'details', 'tv', this.providerGroup, 1)
+      this.util.relatedInfo(this.maxTvShows, this.hboTvCredits, 'credits', 'tv', this.providerGroup, 1)
+      this.util.relatedInfo(this.huluTvShows, this.huluTvDetails, 'details', 'tv', this.providerGroup, 2)
+      this.util.relatedInfo(this.huluTvShows, this.huluTvCredits, 'credits', 'tv', this.providerGroup, 2)
+      this.util.relatedInfo(this.appleTvShows, this.appleTvDetails, 'details', 'tv', this.providerGroup, 3)
+      this.util.relatedInfo(this.appleTvShows, this.appleTvCredits, 'credits', 'tv', this.providerGroup, 3)
       this.submitting = false
       this.util.loadingMore = false
     } catch (e) {
